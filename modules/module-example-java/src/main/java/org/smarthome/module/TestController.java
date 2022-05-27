0f31ba69bb8a7f257b14fb8f52e826f7dc4b0280
Al_Sah <aplexidr@gmail.com>
2022-05-27 21:33:34 +0300

    modules(java-example): Update test controller

--- modules/module-example-java/src/main/java/org/smarthome/module/TestController.java
+++ modules/module-example-java/src/main/java/org/smarthome/module/TestController.java
@@ -4,8 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.smarthome.sdk.models.Command;
 import org.smarthome.sdk.module.producer.ModuleProducer;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,10 +18,9 @@ public class TestController {
         this.moduleProducer = moduleProducer;
     }
 
-    @PostMapping(path = "/send/{task}")
-    public void sendCommand(@PathVariable String task) {
-        logger.info("Received task: " + task);
-        moduleProducer.sendSync(new Command("device1", task, Command.getFuture(30)), "hub1");
-        // curl -X POST http://localhost:8080/send/data
+    @PostMapping(path = "/send")
+    public void sendCommand(@RequestBody Command command) {
+        logger.info("Received command: " + command.toString());
+        moduleProducer.sendSync(command);
     }
 }
