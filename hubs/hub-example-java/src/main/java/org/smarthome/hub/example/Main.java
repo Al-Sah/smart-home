907838ea3ad5fc8d58869cb2082a272e8135284d
Al_Sah <aplexidr@gmail.com>
2022-05-30 22:33:35 +0300

    hubs(java-sample): Update sdk version
    + add log4j2.properties

--- hubs/hub-example-java/src/main/java/org/smarthome/hub/example/Main.java
+++ hubs/hub-example-java/src/main/java/org/smarthome/hub/example/Main.java
@@ -1,120 +1,58 @@
 package org.smarthome.hub.example;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.smarthome.sdk.hub.HubProducerException;
-import org.smarthome.sdk.models.DeviceData;
-import org.smarthome.sdk.models.HubMessage;
-
-import org.smarthome.sdk.hub.HubProducer;
+import org.smarthome.sdk.hub.producer.DeviceMessageCallback;
+import org.smarthome.sdk.hub.producer.HubProducer;
+import org.smarthome.sdk.hub.producer.HubProducerConfiguration;
+import org.smarthome.sdk.hub.producer.HubProducerException;
+import org.smarthome.sdk.models.DeviceDisconnectionDetails;
 import org.smarthome.sdk.models.MessageAction;
-import org.smarthome.sdk.models.ProducerConfiguration;
 
-import java.util.*;
+import java.io.IOException;
+import java.util.Properties;
 
 public class Main {
 
     public static HubProducer producer;
-    public static String hubId = "java-hub-sample-use-sdk";
-    public static String topic = "hubs-messages";
-
-    // TODO logger config
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(final String[] args) {
 
-        var sensor1 = new SensorImitator(UUID.randomUUID().toString(), "thermometer", "temperature imitator1");
-        var sensor2 = new SensorImitator(UUID.randomUUID().toString(), "thermometer", "temperature imitator2");
-
-
         try {
-            setupProducer(new HubMessage<>(MessageAction.HUB_START, "test-hub"));
+            var producerProperties = new Properties();
+            producerProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+            var producerConfiguration = new HubProducerConfiguration(
+                    "hubs-messages", "java-hub-sample", producerProperties, "test-hub"
+            );
+            producer = new HubProducer(producerConfiguration);
         } catch (HubProducerException e) {
             System.out.println(e.getMessage());
             return;
         }
 
+        var testSensor = new TestDevice("test-device", new DeviceMessageCallback(producer));
 
         try {
-            producer.send(
-                    createDevicesConnectedMessage(List.of(sensor1, sensor2)),
-                    (event, ex)-> System.out.println(event));
-        } catch (HubProducerException e) {
-            System.out.println(e.getMessage());
-        }
-
-
-        for (int i = 0; i < 10; i++) {
-            try {
-                producer.send(
-                        createDevicesDataMessage(List.of(sensor1, sensor2)),
-                        (event, ex)-> System.out.println(event));
-                Thread.sleep(3000);
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-        }
-
-
-        try {
-            producer.send(
-                    new HubMessage<>(MessageAction.HUB_MESSAGE, "hub is shutting down"),
-                    (event, ex)-> System.out.println(event));
+            producer.registerDevice(testSensor);
+            producer.setHeartBeatData("1");
         } catch (HubProducerException e) {
             System.out.println(e.getMessage());
         }
 
         try {
-            producer.send(
-                    createDevicesDisconnectedMessage(List.of(sensor1, sensor2)),
-                    (event, ex)-> System.out.println(event));
-        } catch (HubProducerException e) {
-            System.out.println(e.getMessage());
-        }
+            System.out.println("Hit Enter to terminate...");
+            System.in.read();
+            System.out.println("Hub is shutting down ....");
 
+            testSensor.stop();
+            producer.send(MessageAction.DEVICE_DISCONNECTED,
+                    new DeviceDisconnectionDetails(testSensor.getId(), "hub is going to shutdown")
+            );
+            producer.stop("user requested shutdown", null, (event, ex)-> System.out.println(event));
 
-        try {
-            producer.stop((event, ex)-> System.out.println(event), "test-end");
-        } catch (HubProducerException e) {
+        } catch (HubProducerException | IOException e) {
             throw new RuntimeException(e);
         }
 
-    }
-
-    public static HubMessage<List<DeviceData>> createDevicesConnectedMessage(List<SensorImitator> sensors){
-
-        var data = new ArrayList<DeviceData>();
-        for (SensorImitator sensor : sensors) {
-            data.add(new DeviceData(sensor.getId(), sensor.getType(), sensor.getName(), "{\"unit\":\"celsius\"}"));
-        }
-        return new HubMessage<>(MessageAction.DEVICES_CONNECTED, data);
-    }
-
-    public static HubMessage<List<DeviceData>> createDevicesDisconnectedMessage(List<SensorImitator> sensors){
-
-        var data = new ArrayList<DeviceData>();
-        for (SensorImitator sensor : sensors) {
-            data.add(new DeviceData(sensor.getId(), "Connection lost"));
-        }
-        return new HubMessage<>(MessageAction.DEVICES_DISCONNECTED, data);
-    }
-
-    public static HubMessage<List<DeviceData>> createDevicesDataMessage(List<SensorImitator> sensors){
-
-        var data = new ArrayList<DeviceData>();
-        for (SensorImitator sensor : sensors) {
-            data.add(new DeviceData(sensor.getId(), sensor.getData()));
-        }
-        return new HubMessage<>(MessageAction.DEVICE_MESSAGE, data);
-    }
-
-    public static void setupProducer(HubMessage<String> initialMessage) throws HubProducerException {
-
-        var properties = new Properties();
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        var producerConfiguration = new ProducerConfiguration(topic, hubId, properties, 0, (short)10,null);
-        producer = new HubProducer(producerConfiguration, initialMessage);
     }
 
 }
---
+++ hubs/hub-example-java/src/main/java/org/smarthome/hub/example/TestDevice.java
@@ -0,0 +1,53 @@
+package org.smarthome.hub.example;
+
+import org.smarthome.sdk.hub.device.ConstantDeviceProperty;
+import org.smarthome.sdk.hub.device.Device;
+import org.smarthome.sdk.hub.device.DeviceComponent;
+import org.smarthome.sdk.hub.device.WritableDeviceProperty;
+import org.smarthome.sdk.hub.producer.DeviceCallback;
+import org.smarthome.sdk.models.Command;
+import org.smarthome.sdk.models.DeviceType;
+
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class TestDevice extends Device {
+
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+    private final Random random = new Random();
+    public TestDevice(String id, DeviceCallback callback) {
+        super(
+                id,
+                DeviceType.SENSOR,
+                "test-device",
+                new DeviceComponent[]{new DeviceComponent(
+                        id+"-child1",
+                        "test-component1",
+                        "bananas",
+                        new WritableDeviceProperty<Integer>("bananas", "(._.)", "bananas count", null),
+                        new ConstantDeviceProperty[]{
+                                new ConstantDeviceProperty<>("test-ro-prop", "(._.)", "", "(._.)")
+                        },
+                        null)
+                }, callback);
+
+        scheduler.scheduleAtFixedRate(this::imitateWork, 0, 80, TimeUnit.SECONDS);
+    }
+
+    private void imitateWork(){
+        var value = random.nextInt(100);
+        ((WritableDeviceProperty<Integer>)components[0].getMainProperty()).setValue(value);
+        callback.send(id, components[0].getId(), "bananas", Integer.toString(value), null);
+    }
+
+    @Override
+    public void execute(Command command) {}
+
+    @Override
+    public void stop() {
+        scheduler.shutdown();
+    }
+}
