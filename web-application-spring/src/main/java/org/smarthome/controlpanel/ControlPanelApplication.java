59d4d8723c857ee411a32485028ec2c2d892ba7c
Al_Sah <aplexidr@gmail.com>
2022-06-09 16:33:43 +0300

    web-app-java: Setup

---
+++ web-application-spring/src/main/java/org/smarthome/controlpanel/ControlPanelApplication.java
@@ -0,0 +1,13 @@
+package org.smarthome.controlpanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlPanelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControlPanelApplication.class, args);
+    }
+
+}
---
+++ web-application-spring/src/test/java/org/smarthome/controlpanel/ControlPanelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.smarthome.controlpanel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlPanelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
