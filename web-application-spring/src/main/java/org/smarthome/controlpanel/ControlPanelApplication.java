f6186dee3ce8a4c2f5efc3662f23071a3c7316e4
Al_Sah <aplexidr@gmail.com>
2022-06-21 15:42:38 +0300

    web-app-java: Update producer component

--- web-application-spring/src/main/java/org/smarthome/controlpanel/ControlPanelApplication.java
+++ web-application-spring/src/main/java/org/smarthome/controlpanel/ControlPanelApplication.java
@@ -1,26 +1,20 @@
 package org.smarthome.controlpanel;
 
 import org.smarthome.controlpanel.config.HistoryModuleConfiguration;
-import org.smarthome.sdk.module.producer.ProducerProvider;
+import org.smarthome.sdk.module.producer.config.ModuleProducerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = {
         "org.smarthome.sdk.module.producer",
         "org.smarthome.sdk.models",
         "org.smarthome.controlpanel"
 })
-@EnableConfigurationProperties(HistoryModuleConfiguration.class)
+@EnableConfigurationProperties({HistoryModuleConfiguration.class, ModuleProducerConfiguration.class})
 public class ControlPanelApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ControlPanelApplication.class, args);
-    }
-
-    @Bean
-    public ProducerProvider getProducerProvider(){
-        return new ProducerProvider("modules-messages", null, 0);
     }
 }
