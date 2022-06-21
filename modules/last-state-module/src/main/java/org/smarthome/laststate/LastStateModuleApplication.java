0c2464f168792ba53274fb9965b3cd1644d0011b
Al_Sah <aplexidr@gmail.com>
2022-06-21 15:40:53 +0300

    modules(last-state): Update consumer component

--- modules/last-state-module/src/main/java/org/smarthome/laststate/LastStateModuleApplication.java
+++ modules/last-state-module/src/main/java/org/smarthome/laststate/LastStateModuleApplication.java
@@ -1,25 +1,19 @@
 package org.smarthome.laststate;
 
-import org.smarthome.sdk.module.consumer.ConsumerTopicsProvider;
+import org.smarthome.sdk.module.consumer.config.ModuleConsumerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication(scanBasePackages = {
         "org.smarthome.sdk.module.consumer",
         "org.smarthome.sdk.models",
         "org.smarthome.laststate"
 })
+@EnableConfigurationProperties(ModuleConsumerConfiguration.class)
 public class LastStateModuleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LastStateModuleApplication.class, args);
     }
-
-    @Bean
-    ConsumerTopicsProvider listenerTopics() {
-        return new ConsumerTopicsProvider(new String[] {"hubs-messages"});
-    }
-
-
 }
