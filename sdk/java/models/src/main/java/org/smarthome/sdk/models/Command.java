1dc4cbde1051e4de52e0506c350c9e2b3dd051ab
Al_Sah <aplexidr@gmail.com>
2022-06-22 11:36:16 +0300

    java-sdk(models): Update Command DTO

--- sdk/java/models/src/main/java/org/smarthome/sdk/models/Command.java
+++ sdk/java/models/src/main/java/org/smarthome/sdk/models/Command.java
@@ -3,15 +3,20 @@ package org.smarthome.sdk.models;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * <p>This DTO describes command tha will be executed by specified device </p>
+ *
+ * @author Al-Sah
+ */
 public class Command {
 
     private final String hub;
     private final String device;
     private final String component;
     private final String property;
+    private final String value;
     private final String options;
     private final long expiration;
-
 
     @JsonCreator
     public Command(
@@ -19,12 +24,14 @@ public class Command {
             @JsonProperty("device") String device,
             @JsonProperty("component") String component,
             @JsonProperty("property") String property,
+            @JsonProperty("value") String value,
             @JsonProperty("options") String options,
             @JsonProperty("expire") long expiration) {
         this.hub = hub;
         this.device = device;
         this.component = component;
         this.property = property;
+        this.value = value;
         this.options = options;
         this.expiration = expiration;
     }
@@ -44,6 +51,10 @@ public class Command {
 
     public String getDevice() {
         return device;
+    }
+
+    public String getValue() {
+        return value;
     }
 
     public String getProperty() {
