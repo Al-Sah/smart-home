9d8ebfd31793ae7e4e42cab46e626702fdac0e56
SenseyAlan <oleksii.nikolaiev@nure.ua>
2022-06-28 11:32:42 +0300

    Web-App: history page pagination added

--- web-application-spring/src/main/resources/static/js/historyScripts.js
+++ web-application-spring/src/main/resources/static/js/historyScripts.js
@@ -4,9 +4,96 @@
 function getDeviceID() {
     return window.location.search.replace("?","");
 }
-function showHistory(data) {
-    console.log(data);
+let state = {
+    'querySet': "",
+    'page' : 1,
+    'rows': 10,
+    'window': 5,
 }
+function showHistory() {
+    $('.table').prop('hidden', false);
+    buildTable()
+}
+function pagination(querySet, page, rows) {
+
+    let trimStart = (page - 1) * rows
+    let trimEnd = trimStart + rows
+
+    let trimmedData = querySet.slice(trimStart, trimEnd)
+
+    let pages = Math.round(querySet.length / rows);
+
+    return {
+        'querySet': trimmedData,
+        'pages': pages,
+    }
+}
+
+function pageButtons(pages) {
+    let wrapper = document.getElementById('pagination_wrapper')
+    wrapper.innerHTML = ``;
+
+    console.log('Pages:', pages)
+
+    let maxLeft = (state.page - Math.floor(state.window / 2))
+    let maxRight = (state.page + Math.floor(state.window / 2))
+
+    if (maxLeft < 1) {
+        maxLeft = 1
+        maxRight = state.window
+    }
+
+    if (maxRight > pages) {
+        maxLeft = pages - (state.window - 1)
+
+        if (maxLeft < 1) {
+            maxLeft = 1
+        }
+        maxRight = pages
+    }
+
+
+
+    for (let page = maxLeft; page <= maxRight; page++) {
+        wrapper.innerHTML += `<button value=${page} class="page btn btn-sm btn-info">${page}</button>`
+    }
+
+    if (state.page != 1) {
+        wrapper.innerHTML = `<button value=${1} class="page btn btn-sm btn-info">&#171; First</button>` + wrapper.innerHTML
+    }
+
+    if (state.page != pages) {
+        wrapper.innerHTML += `<button value=${pages} class="page btn btn-sm btn-info">Last &#187;</button>`
+    }
+
+    $('.page').on('click', function() {
+        $('#result_table').empty()
+
+        state.page = Number($(this).val())
+
+        buildTable()
+    })
+}
+
+function buildTable() {
+    let table = $('#result_table')
+    table.empty();
+    let data = pagination(state.querySet, state.page, state.rows)
+    let myList = data.querySet;
+    myList.forEach(function (item, key) {
+        let lastDate = new Date(item.ts).toLocaleString()
+        let row = `<tr>
+                  <td>${item.component}</td>
+                  <td>${item.hub}</td>
+                  <td>${item.property}</td>
+                  <td>${Math.ceil(item.value)}</td>
+                  <td>${lastDate}</td>
+                  </tr>`
+        table.append(row)
+    });
+    pageButtons(data.pages)
+}
+
 function getHistory(id, type, TimeFrom, TimeTo) {
     $.ajax({
         url:'http://localhost:8080/history',
@@ -18,31 +105,26 @@ function getHistory(id, type, TimeFrom, TimeTo) {
             $('#btn_getHistory').prop('disabled',true);
         },
         success: function(data) {
-            console.log(typeof (data));
+            state.querySet = data;
+            showHistory();
             $('#btn_getHistory').prop('disabled',false);
-            return data;
         },
         error: function () {
             console.log('error');
             $('#btn_getHistory').prop('disabled',false);
-            return false;
         }
     });
 }
 $(document).ready(function () {
     let deviceId = getDeviceID();
     let deviceType = "device";
-    let timeTo = $('#timeTo_input').val();
     $('#object-id-input').val(deviceId);
     $('#object-type-input').val(deviceType);
     $('#btn_getHistory').on("click",function () {
         let timeFrom = new Date($('#timeFrom_input').val()).getTime();
         let timeTo = new Date($('#timeTo_input').val()).getTime();
         if((timeFrom && timeTo) !== undefined){
-            let data = getHistory(deviceId, deviceType, timeFrom, timeTo);
-            if(data){
-                showHistory(data);
-            }
+            getHistory(deviceId, deviceType, timeFrom, timeTo);
         }
     })
 });
