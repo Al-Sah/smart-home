92c523549f5fb41a78bcd177dfd4ec4ce66103cc
Al_Sah <aplexidr@gmail.com>
2022-06-29 22:03:08 +0300

    web-app(ui): Update history request
    New: user can select device/components

--- web-application-spring/src/main/resources/static/js/historyScripts.js
+++ web-application-spring/src/main/resources/static/js/historyScripts.js
@@ -1,4 +1,26 @@
 
+$(document).ready(function () {
+
+    // setup form
+    let deviceId = new URLSearchParams(window.location.search).get('id');
+    let deviceType = "device";
+    $('#object-id-input').val(deviceId);
+    $('#object-type-input').val(deviceType);
+
+
+    $('#get-history-button').on("click",function () {
+        // FIXME
+        let timeFrom = new Date($('#time-from-input').val()).getTime();
+        let timeTo = new Date($('#time-to-input').val()).getTime();
+
+        if((timeFrom && timeTo) !== undefined){
+            getHistory($('#object-id-input').val(), $('#object-type-input').val(), timeFrom, timeTo);
+        }
+    })
+    getDeviceStructure()
+});
+
+
 let state = {
     'querySet': "",
     'page' : 1,
@@ -6,23 +28,13 @@ let state = {
     'window': 5,
 }
 
-function showHistory() {
-    $('.table').prop('hidden', false);
-    buildTable()
-}
-
 function pagination(querySet, page, rows) {
 
     let trimStart = (page - 1) * rows
     let trimEnd = trimStart + rows
-
-    let trimmedData = querySet.slice(trimStart, trimEnd)
-
-    let pages = Math.round(querySet.length / rows);
-
     return {
-        'querySet': trimmedData,
-        'pages': pages,
+        'querySet': querySet.slice(trimStart, trimEnd),
+        'pages': Math.round(querySet.length / rows),
     }
 }
 
@@ -55,19 +67,17 @@ function pageButtons(pages) {
         wrapper.innerHTML += `<button value=${page} class="page btn btn-sm btn-info">${page}</button>`
     }
 
-    if (state.page != 1) {
+    if (state.page !== 1) {
         wrapper.innerHTML = `<button value=${1} class="page btn btn-sm btn-info">&#171; First</button>` + wrapper.innerHTML
     }
 
-    if (state.page != pages) {
+    if (state.page !== pages) {
         wrapper.innerHTML += `<button value=${pages} class="page btn btn-sm btn-info">Last &#187;</button>`
     }
 
     $('.page').on('click', function() {
         $('#result_table').empty()
-
         state.page = Number($(this).val())
-
         buildTable()
     })
 }
@@ -77,7 +87,7 @@ function buildTable() {
     table.empty();
     let data = pagination(state.querySet, state.page, state.rows)
     let myList = data.querySet;
-    myList.forEach(function (item, key) {
+    myList.forEach(function (item) {
         let lastDate = new Date(item.ts).toLocaleString()
         let row = `<tr>
                   <td>${item.component}</td>
@@ -99,36 +109,21 @@ function getHistory(id, type, TimeFrom, TimeTo) {
         data:{'type' : type, 'id' : id, 'from': TimeFrom, 'to': TimeTo},
         dataType: 'json',
         beforeSend: function() {
-            $('#btn_getHistory').prop('disabled',true);
+            $('#get-history-button').prop('disabled',true);
         },
         success: function(data) {
             state.querySet = data;
-            showHistory();
-            $('#btn_getHistory').prop('disabled',false);
+            $('.table').prop('hidden', false);
+            buildTable()
+            $('#get-history-button').prop('disabled',false);
         },
         error: function () {
             console.log('error');
-            $('#btn_getHistory').prop('disabled',false);
+            $('#get-history-button').prop('disabled',false);
         }
     });
 }
 
-$(document).ready(function () {
-
-    let deviceId = new URLSearchParams(window.location.search).get('id');
-    let deviceType = "device";
-
-    $('#object-id-input').val(deviceId);
-    $('#object-type-input').val(deviceType);
-    $('#btn_getHistory').on("click",function () {
-        let timeFrom = new Date($('#timeFrom_input').val()).getTime();
-        let timeTo = new Date($('#timeTo_input').val()).getTime();
-        if((timeFrom && timeTo) !== undefined){
-            getHistory(deviceId, deviceType, timeFrom, timeTo);
-        }
-    })
-    getDeviceStructure()
-});
 
 
 function getDeviceStructure(){
@@ -139,7 +134,7 @@ function getDeviceStructure(){
         return;
     }
 
-    let device = $('#deviceStructure')[0];
+    let device = $('#device-structure')[0];
     $.ajax({
         url:'http://localhost:8083/device',
         type: 'GET',
@@ -147,7 +142,64 @@ function getDeviceStructure(){
         data: {'id' : deviceId},
         crossDomain: true,
         beforeSend: ()=> device.innerHTML = "<p> Loading ... </p>",
-        success: ()=> device.innerHTML = "<p> Loaded </p>",
+        success: (item)=> device.innerHTML = printDeviceStructure(item.metadata),
         error:  ()=> device.innerHTML = "<p> Failed to load structure </p>",
     });
+}
+
+function printDeviceStructure(structure){
+
+    return `
+        <div class="list-group shadow-sm p-3 mb-5 bg-white rounded">
+            <a href="#" class="list-group-item list-group-item-action border border-light rounded" onclick="updateFormInputs('${structure.id}','device')">
+                <p class="text-primary text-center"> 
+                     ${structure.name} <br> <small class="text-secondary"> ${structure.id}</small>
+                </p>
+                <div class="list-group">
+                    ${generateComponentsList(structure.components)}
+                </div>
+            </a>
+        </div>
+    `;
+}
+
+function generateComponentsList(components){
+
+    let res = ``;
+    components.forEach(function (component) {
+        let mainProperty = component.mainProperty;
+        res+=`
+        <a href="#" class="list-group-item list-group-item-info list-group-item-action mb-1" onclick="updateFormInputs('${component.id}','component')"> 
+            <div class="mb-1">
+                <p class="text-primary"> 
+                    ${mainProperty.name};  unit: '${mainProperty.unit}' <br> 
+                    <small> ${mainProperty.description} </small> 
+                </p>
+                ${generatePropertiesList(component.constProperties, 'constant')}
+                ${generatePropertiesList(component.writableProperties, 'writable')}
+            </div>
+        </a>`;
+    });
+    return res;
+}
+
+function updateFormInputs(id, type){
+    $('#object-id-input').val(id);
+    $('#object-type-input').val(type);
+}
+
+function generatePropertiesList(properties, type){
+
+    if(properties === null || properties.length === 0){
+        return `<p>No ${type} properties specified</p>`;
+    }
+
+    let listItems = ``;
+    properties.forEach(function (property) {
+        listItems += `
+            <li class="list-group-item list-group-item-secondary">
+                <p>${property.name} '${property.unit}' <br> <small> ${property.description} </small></p>
+            </li>`
+    })
+    return `<p> ${type} properties: </p><ul class="list-group">${listItems}</ul>`
 }
